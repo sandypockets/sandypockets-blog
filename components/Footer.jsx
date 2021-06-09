021ee1526ab2c490aed8487f359bb22175a31dbe
sandypockets <zac@znacreative.com>
2021-06-09 18:31:09 -0400

    Map over footer menu items

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,30 @@
+import Link from "next/link";
+
+const footerItems = [
+  {
+    name: "Home",
+    href: "/"
+  },
+  {
+    name: "About",
+    href: "/about"
+  },
+  {
+    name: "Blog",
+    href: "/blog"
+  },
+]
+
 export default function Footer() {
   return (
     <div className="w-full flex justify-center bg-black text-white py-12">
-      <p>Link!</p>
-      <p>Link!</p>
-      <p>Link!</p>
+
+      {footerItems.map((item) => (
+        <Link key={item.name} href={item.href}>
+          <a className="mr-11 nav-hover">{item.name}</a>
+        </Link>
+      ))}
+
     </div>
   )
 }
