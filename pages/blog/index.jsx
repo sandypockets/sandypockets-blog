9c07eec10a1cc33804e7c4593dabfbb4754e49ac
sandypockets <zac@znacreative.com>
2021-06-09 23:27:26 -0400

    Add title and posts to page

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,7 +1,16 @@
+import {posts} from "../../getAllPosts";
+import {Post} from "../../components/hero/Post";
+import Highlight from "../../components/utility/Highlight";
+
 export default function Index() {
   return (
     <div>
-      Some Blog Things
+      <h1 className="flex justify-center h-auto pb-11 text-8xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 via-blue-500 to-purple-600">Blog</h1>
+      <div className="mb-24">
+        {posts.map((post) => (
+          <Post key={post.link} post={post} />
+        ))}
+      </div>
     </div>
   )
 }
