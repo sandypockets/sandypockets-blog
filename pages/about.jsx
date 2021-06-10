1d567065c2e11a4363220ed35a5c6183a46a4cf4
sandypockets <zac@znacreative.com>
2021-06-10 00:01:06 -0400

    Add placeholder text as styling aid

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,7 +1,12 @@
 export default function About() {
   return (
-    <div>
-      Some things!
+    <div className="my-11 mx-48">
+      <h1 className="flex justify-center h-auto pb-11 text-8xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 via-blue-500 to-purple-600">
+        About
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
     </div>
   )
 }
