401d29df5ab1fbb9202d6f0d9a50bf3fd953057e
sandypockets <zac@znacreative.com>
2021-06-09 22:40:32 -0400

    Darken text colour

--- components/article/Date.jsx
+++ components/article/Date.jsx
@@ -1,3 +1,3 @@
 export default function Date(props) {
-  return <span className="text-purple-500 pr-5">{props.meta.date}</span>
+  return <span className="text-purple-600 pr-5">{props.meta.date}</span>
 }
--- components/article/ReadingTime.jsx
+++ components/article/ReadingTime.jsx
@@ -1,6 +1,6 @@
 export default function ReadingTime(props) {
   return (
-    <span className="text-md text-purple-500">
+    <span className="text-md text-purple-600">
         {props.meta.readTime + ' min read'}
       </span>
   )
