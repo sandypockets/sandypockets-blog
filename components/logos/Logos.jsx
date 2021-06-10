ff628288f60a9ccdcd8c59fcc5471767daf46ffb
sandypockets <zac@znacreative.com>
2021-06-10 00:53:28 -0400

    Add svg logos

---
+++ components/logos/Logos.jsx
@@ -0,0 +1,48 @@
+import ReactLogo from "./ReactLogo";
+import JsLogo from "./JsLogo"
+import RubyLogo from "./RubyLogo"
+import RailsLogo from "./RailsLogo";
+import HtmlLogo from "./HtmlLogo";
+import CssLogo from "./CssLogo";
+import NodeLogo from "./NodeLogo";
+import PostgresqlLogo from "./PostgresqlLogo";
+import SqlLiteLogo from "./SqlLiteLogo";
+import MarkdownLogo from "./MarkdownLogo";
+import ExpressLogo from "./ExpressLogo";
+import BootstrapLogo from "./BootstrapLogo";
+import ChaiLogo from "./ChaiLogo";
+import CypressLogo from "./CypressLogo";
+import JestLogo from "./JestLogo";
+import JQueryLogo from "./JQueryLogo";
+import NextJsLogo from "./NextJsLogo";
+import MochaLogo from "./MochaLogo";
+import SassLogo from "./SassLogo";
+import StorybookLogo from "./StorybookLogo";
+import TailwindLogo from "./TailwindLogo";
+
+export default function Logos() {
+  return (
+    <div className="flex justify-center flex-wrap m-24">
+      <BootstrapLogo />
+      <ChaiLogo />
+      <CssLogo />
+      <CypressLogo />
+      <ExpressLogo />
+      <HtmlLogo />
+      <JestLogo />
+      <JQueryLogo />
+      <MarkdownLogo />
+      <NextJsLogo />
+      <MochaLogo />
+      <NodeLogo />
+      <PostgresqlLogo />
+      <RailsLogo />
+      <ReactLogo />
+      <RubyLogo />
+      <SassLogo />
+      <SqlLiteLogo />
+      <StorybookLogo />
+      <TailwindLogo />
+    </div>
+  )
+}
