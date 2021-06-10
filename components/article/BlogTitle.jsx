f87154ad466312f754be6ec70380f2b1ba8da35c
sandypockets <zac@znacreative.com>
2021-06-09 22:52:29 -0400

    Use gradient on text hover

--- components/article/BlogTitle.jsx
+++ components/article/BlogTitle.jsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 export default function BlogTitle(props) {
   const [hover, setHover] = useState(false)
 
-  const handleMouseOver = (hover) => {
+  const handleMouseOver = () => {
    setHover(true)
   }
 
@@ -14,26 +14,40 @@ export default function BlogTitle(props) {
 
   return (
     <div>
-      {!hover ?
-        <h1
-          className={props.propsClass}
-          onMouseOver={() => handleMouseOver()}
-          onMouseOut={() => handleMouseOut()}
-        >
-          <Link href={props.meta.href}>
-            <a>{props.meta.title}</a>
-          </Link>
-        </h1>
-        :
-        <h1 className={props.propsClass + " text-transparent bg-clip-text bg-gradient-to-r from-green-400 via-blue-500 to-purple-600"}
-          onMouseOver={() => handleMouseOver()}
-          onMouseOut={() => handleMouseOut()}
-          >
-          <Link href={props.meta.href}>
-            <a>{props.meta.title}</a>
-          </Link>
-        </h1>
-      }
+
+      <h1
+        className={hover ? props.propsClass + " text-transparent bg-clip-text bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 " + "transition duration-800 ease-in-out" : props.propsClass}
+        onMouseOver={() => handleMouseOver()}
+        onMouseOut={() => handleMouseOut()}
+      >
+        <Link href={props.meta.href}>
+          <a>{props.meta.title}</a>
+        </Link>
+      </h1>
+
+
+      {/*{!hover ?*/}
+      {/*  <h1*/}
+      {/*    className={!hover ? props.propsClass + " transition duration-600 ease-in-out" : props.propsClass + " text-transparent bg-clip-text bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 " + "transition duration-500 ease-in-out"}*/}
+      {/*    onMouseOver={() => handleMouseOver()}*/}
+      {/*    onMouseOut={() => handleMouseOut()}*/}
+      {/*  >*/}
+      {/*    <Link href={props.meta.href}>*/}
+      {/*      <a>{props.meta.title}</a>*/}
+      {/*    </Link>*/}
+      {/*  </h1>*/}
+      {/*  :*/}
+      {/*  <h1 className={props.propsClass + " text-transparent bg-clip-text bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 " + "transition duration-500 ease-in-out"}*/}
+      {/*    onMouseOver={() => handleMouseOver()}*/}
+      {/*    onMouseOut={() => handleMouseOut()}*/}
+      {/*    >*/}
+      {/*    <Link href={props.meta.href}>*/}
+      {/*      <a>{props.meta.title}</a>*/}
+      {/*    </Link>*/}
+      {/*  </h1>*/}
+      {/*}*/}
+
+
     </div>
   )
 }
