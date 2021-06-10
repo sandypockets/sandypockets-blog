4d22edbcd365d008029b207fd22f7085b1fd4879
sandypockets <zac@znacreative.com>
2021-06-09 22:21:36 -0400

    Add hover to h1 link

--- components/article/BlogTitle.jsx
+++ components/article/BlogTitle.jsx
@@ -1,11 +1,42 @@
 import Link from "next/link";
+import { useState } from 'react'
 
 export default function BlogTitle(props) {
+  const [hover, setHover] = useState(false)
+
+  const handleMouseOver = (hover) => {
+   setHover(true)
+  }
+
+  const handleMouseOut = () => {
+    setHover(false)
+  }
+
   return (
-    <h1 className={props.propsClass}>
-      <Link href={props.meta.href}>
-        <a>{props.meta.title}</a>
-      </Link>
-    </h1>
+    <div>
+      {!hover ?
+        <h1
+          className={props.propsClass}
+          onMouseOver={() => handleMouseOver()}
+          onMouseOut={() => handleMouseOut()}
+        >
+          <Link href={props.meta.href}>
+            <a>{props.meta.title}</a>
+          </Link>
+        </h1>
+        :
+        <h1 className={props.propsClass + " text-transparent bg-clip-text bg-gradient-to-r from-green-400 via-blue-500 to-purple-600"}
+          onMouseOver={() => handleMouseOver()}
+          onMouseOut={() => handleMouseOut()}
+          >
+          <Link href={props.meta.href}>
+            <a>{props.meta.title}</a>
+          </Link>
+        </h1>
+      }
+
+
+
+    </div>
   )
 }
