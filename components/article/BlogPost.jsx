4de9439a55b7aaa1f6c02f62d36cb5ebac588a83
sandypockets <zac@znacreative.com>
2021-06-09 23:43:40 -0400

    Use grey text instead of white for readability

--- components/article/BlogPost.jsx
+++ components/article/BlogPost.jsx
@@ -5,9 +5,9 @@ export default function BlogPost({ children, meta}) {
   return (
     <>
       <div className="my-11 mx-48">
-        <ArticleTitle propsClass="h-auto pb-11 text-8xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500" meta={meta} />
+        <ArticleTitle propsClass="flex justify-center h-auto pb-11 text-8xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 via-blue-500 to-purple-600" meta={meta} />
         <Description isBlogPost meta={meta} />
-        <article className="mt-4">{children}</article>
+        <article className="mt-4 flex justify-center text-gray-300">{children}</article>
       </div>
     </>
   )
