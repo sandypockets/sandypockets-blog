9dd995ac249d7f8843307b72aae2f3cd2e7a7b37
sandypockets <zac@znacreative.com>
2021-06-11 21:28:07 -0400

    Add breakpoints

--- components/hero/HeroPost.jsx
+++ components/hero/HeroPost.jsx
@@ -5,7 +5,7 @@ import Description from "../article/Description";
 
 export const HeroPost = ({ meta, isBlogPost }) => (
   <>
-    <BlogTitle propsClass="text-4xl font-mono" meta={meta} />
+    <BlogTitle propsClass="font-mono text-2xl sm:text-4xl md:text-5xl" meta={meta} />
     <div className="font-mono">
       <Description isBlogPost={isBlogPost} meta={meta} />
       <Date meta={meta} />
