b6bf4ff52b86b78554fc4180610530dec1aa3c00
sandypockets <zac@znacreative.com>
2021-06-11 23:57:45 -0400

    Use Highlight component

--- components/article/ArticleTitle.jsx
+++ components/article/ArticleTitle.jsx
@@ -1,10 +1,12 @@
+import Highlight from "../utility/Highlight";
+
 export default function ArticleTitle(props) {
   return (
     <div>
       <h1
         className={props.propsClass}
       >
-        {props.meta.title}
+        <Highlight content={props.meta.title} />
       </h1>
     </div>
   )
