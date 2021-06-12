c907a547d5aa08e04eeaa42066463499d214953a
sandypockets <zac@znacreative.com>
2021-06-12 00:01:38 -0400

    Adjust h1 breakpoints

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -5,7 +5,7 @@ import Highlight from "../../components/utility/Highlight";
 export default function Index() {
   return (
     <div>
-      <h1 className="flex justify-center h-auto pb-11 text-8xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 via-blue-500 to-purple-600">Blog</h1>
+      <h1 className="flex justify-center h-40 md:h-44 pb-11 text-8xl font-bold "><Highlight content="Blog" /></h1>
       <div className="mb-24">
         {posts.map((post) => (
           <Post key={post.link} post={post} />
