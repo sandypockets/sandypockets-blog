7f2783bc004e16527f140f51312e227fa9ee216b
sandypockets <zac@znacreative.com>
2021-06-12 00:42:40 -0400

    Adjust section spacing at various breakpoints

--- pages/about.jsx
+++ pages/about.jsx
@@ -4,8 +4,8 @@ import Highlight from "../components/utility/Highlight";
 
 export default function About() {
   return (
-    <div className="sm:my-11 sm:mx-22 md:mx-8 lg:mx-14 xl:mx-32 2xl:mx-36">
-      <div className="text-3xl sm:text-8xl sm:leading-20 font-bold text-gray-100">
+    <div className="sm:my-11 sm:mx-22 md:mx-8 lg:mx-14 xl:mx-32 2xl:max-w-7xl">
+      <div className="text-3xl sm:text-6xl sm:leading-20 sm:mb-20 md:text-7xl xl:text-8xl 2xl:text-9xl xl:pl-14 2xl:pl-16 font-bold text-gray-100">
         <p>I'm a serial learner,{' '}
           <br/>
           helping to build a <Highlight content="safe"/>, <Highlight content="open"/>, and <Highlight content="inclusive"/> internet.</p>
