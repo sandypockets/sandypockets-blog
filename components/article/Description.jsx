75936453226a1078c1c538b03d20e528fa171f8c
sandypockets <zac@znacreative.com>
2021-06-11 21:27:46 -0400

    Add breakpoints

--- components/article/Description.jsx
+++ components/article/Description.jsx
@@ -1,6 +1,6 @@
 export default function Description(props) {
   return (
-    <div>
+    <div className="text-sm my-1 sm:my-2 sm:text-lg md:text-xl">
       {props.isBlogPost? null: <p>{props.meta.description}</p>}
     </div>
   )
