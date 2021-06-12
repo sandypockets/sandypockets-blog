b33757e3f6f5c286bb241f43f17c4f8f7c009d88
sandypockets <zac@znacreative.com>
2021-06-11 21:12:58 -0400

    Make responsive

--- components/layout/Nav.jsx
+++ components/layout/Nav.jsx
@@ -13,13 +13,13 @@ const navItems = [
 
 export default function Nav() {
   return (
-    <div className="flex justify-between mb-5 w-full fixed sm:w-11/12 md:w-11/12 lg:w-11/12 sm:py-6 md:py-6 lg:py-6 font-mono">
+    <div className="flex justify-between w-full fixed w-11/12 pl-8 py-6 font-mono">
 
       <Link href='/'>
         <a className="flex justify-start nav-hover">SANDYPOCKETS</a>
       </Link>
 
-      <div className="flex justify-evenly sm:px-3 md:px-8">
+      <div className="flex justify-evenly">
 
         {navItems.map((item) => (
           <Link key={item.name} href={item.href}>
