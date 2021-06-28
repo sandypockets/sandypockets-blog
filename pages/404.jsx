558fa1d191688d7d2ab59dba6ff3bf8f2b3557f6
sandypockets <zac@znacreative.com>
2021-06-27 21:21:59 -0400

    Increase font weight

--- pages/404.jsx
+++ pages/404.jsx
@@ -3,9 +3,8 @@ import Highlight from "../components/utility/Highlight";
 export default function Custom404() {
   return (
     <div className="flex flex-col m-4">
-      <Highlight propsClass="text-9xl text-center" content="404" />
-      <Highlight propsClass="text-5xl text-center h-20" content="Page not found" />
-      <br/>
+      <Highlight propsClass="text-9xl font-bold text-center" content="404" />
+      <Highlight propsClass="text-5xl text-center h-20" content="Page not found." />
     </div>
   )
 }
--- pages/500.jsx
+++ pages/500.jsx
@@ -3,9 +3,8 @@ import Highlight from "../components/utility/Highlight";
 export default function Custom500() {
   return (
     <div className="flex flex-col m-4">
-      <Highlight propsClass="text-9xl text-center" content="500" />
-      <Highlight propsClass="text-5xl text-center h-20" content="Server-side error occurred" />
-      <br/>
+      <Highlight propsClass="text-9xl font-bold text-center" content="500" />
+      <Highlight propsClass="text-5xl text-center h-20" content="A server-side error occurred." />
     </div>
   )
 }
