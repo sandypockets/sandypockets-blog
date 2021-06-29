f182ec256d59fa932e9c7efd6ab6f6abdd36cf94
sandypockets <zac@znacreative.com>
2021-06-29 13:08:13 -0400

    ✨ Add grayscale effect to nav scroll

--- components/layout/Nav.jsx
+++ components/layout/Nav.jsx
@@ -13,25 +13,20 @@ const navItems = [
 ]
 
 export default function Nav({ darkMode, setDarkMode }) {
+  const blurStyles = 'backdrop-filter backdrop-grayscale'
   return (
-    <div className="flex justify-between w-full fixed w-11/12 px-8 py-6 font-mono">
-
+    <div className={'flex justify-between w-full fixed w-11/12 h-10 px-8 py-6 font-mono ' + blurStyles}>
       <Link href='/'>
         <a className="flex justify-start nav-hover">SANDYPOCKETS</a>
       </Link>
-
       <div className="flex justify-evenly">
-
         {navItems.map((item) => (
           <Link key={item.name} href={item.href}>
             <a className="mr-11 nav-hover">{item.name}</a>
           </Link>
         ))}
-
         <Toggle darkMode={darkMode} setDarkMode={setDarkMode} />
-
       </div>
-
     </div>
   )
 }
