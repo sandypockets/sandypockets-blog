1aec50ebe14ff60cbe9c005e9ef9c0fe060480a5
sandypockets <zac@znacreative.com>
2021-06-29 23:05:40 -0400

    🐛 Fix title padding

--- components/article/BlogPost.jsx
+++ components/article/BlogPost.jsx
@@ -4,7 +4,7 @@ import Description from "./Description";
 export default function BlogPost({ children, meta }) {
   return (
     <>
-      <div className="mb-10 sm:mt-11 lg:max-w-3xl xl:max-w-4xl 2xl:max-w-5xl">
+      <div className="my-12 sm:mt-11 lg:max-w-3xl xl:max-w-4xl 2xl:max-w-5xl">
         <ArticleTitle propsClass="flex justify-center h-auto pb-6 pt-10 text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 sm:pb-7 sm:text-7xl lg:text-8xl 2xl:text-9xl " meta={meta} />
         <Description isBlogPost meta={meta} />
         <article className="mt-4 flex flex-col justify-center">{children}</article>
