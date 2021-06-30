4ce58e5c832cb508becc83fb64d79b4fa89d5da8
sandypockets <zac@znacreative.com>
2021-06-29 23:00:30 -0400

    ✨ Sort blog posts by id

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,6 +4,8 @@ import HeroText from "../components/hero/HeroText";
 import Tagline from "../components/hero/Tagline";
 
 export default function IndexPage() {
+
+  console.log(posts)
   return (
     <div className="pt-16">
       <div className='sm:mt-20 md:mt-24 lg:mt-30'>
@@ -11,8 +13,11 @@ export default function IndexPage() {
         <Tagline />
       </div>
       <div className="mb-12 sm:mb-24">
-        {posts.map((post) => (
-          <Post key={post.link} post={post} />
+
+        {posts.sort((a ,b) => {
+          return b.module.meta.id - a.module.meta.id
+        }).map((post) => (
+          <Post key={post.module.meta.id} post={post} />
         ))}
       </div>
     </div>
