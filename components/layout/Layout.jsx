d48725af6baa4036350d71a11640e77323da86d5
sandypockets <zac@znacreative.com>
2021-06-29 23:01:50 -0400

    🐛 Fix height and margins

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Nav from "./Nav";
 import Footer from "./Footer";
-import SectionSeparator from "../utility/SectionSeparator";
 
 export default function Layout({ children, pageTitle, description, backgroundColour, textColour, secondaryTextColour, darkMode, setDarkMode }) {
   return (
@@ -12,12 +11,11 @@ export default function Layout({ children, pageTitle, description, backgroundCol
         <meta name="Description" content={description} />
         <title>{pageTitle}</title>
       </Head>
-      <div className="h-screen">
+      <div className={backgroundColour + " " + textColour + " h-screen"}>
         <main className={backgroundColour + " " + textColour + " w-full flex justify-center"}>
           <Nav darkMode={darkMode} setDarkMode={setDarkMode} />
           <div className="content mx-7 sm:mx-16">{children}</div>
         </main>
-        {/*<SectionSeparator className='w-3/5 m-auto mt-6 sm:mt-24' />*/}
         <Footer backgroundColour={backgroundColour} textColour={textColour} secondaryTextColour={secondaryTextColour} />
       </div>
     </>
